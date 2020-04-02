2e46da73b6fa2d4686e035818e981bc5fe891e69
milkatemelkova <50911887+milkatemelkova@users.noreply.github.com>
2020-04-02 22:53:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = "aedkldajduwyw89738642gjdk";
+
+
+
+class App extends Component {
+  state = {
+
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://foodtofork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
+
+const data = await api_call.json();
+this.setState({ recipes: data.recipes });
+console.log()
+  }
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes});
+  }
+  
+  
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}  />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
